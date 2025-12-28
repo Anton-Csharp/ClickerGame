6cc81c57e2c75cdb54a1ea8ef909b40fbf5e3344
Anton-Csharp <AntonCheh456@gmail.com>
2025-12-28 21:10:33 +0300

    добавил основу

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -44,6 +44,15 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label16 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -59,7 +68,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(207, 64);
+            this.label1.Location = new System.Drawing.Point(207, 96);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 1;
@@ -77,7 +86,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(356, 302);
+            this.button3.Location = new System.Drawing.Point(210, 302);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(80, 59);
             this.button3.TabIndex = 3;
@@ -93,7 +102,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(366, 286);
+            this.label2.Location = new System.Drawing.Point(239, 281);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 4;
@@ -111,7 +120,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(366, 364);
+            this.label4.Location = new System.Drawing.Point(228, 364);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(58, 13);
             this.label4.TabIndex = 6;
@@ -162,7 +171,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(385, 107);
+            this.label10.Location = new System.Drawing.Point(366, 124);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(99, 13);
             this.label10.TabIndex = 12;
@@ -171,16 +180,107 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(416, 141);
+            this.label11.Location = new System.Drawing.Point(338, 143);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.Size = new System.Drawing.Size(161, 13);
             this.label11.TabIndex = 13;
+            this.label11.Text = "купи 15 раз бафы в магазине:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(167, 206);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(139, 13);
+            this.label12.TabIndex = 14;
+            this.label12.Text = "ваши клики за 1 нажатие:\r\n";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(29, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "престиж";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 216);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(114, 52);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "для престижа нужно:\r\n5000 опыта\r\n100к кликов\r\n20 раз купить баф\r\n";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(207, 9);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 13);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "label14";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(390, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(387, 268);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(105, 26);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "      мини-игра:\r\nугадай число от 1-5\r\n";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(403, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "нажать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(407, 364);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(64, 13);
+            this.label16.TabIndex = 21;
+            this.label16.Text = "250 кликов";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(369, 242);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 144);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "для открытия нужно \r\n999 опыта";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(511, 398);
+            this.ClientSize = new System.Drawing.Size(555, 398);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -219,6 +319,15 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,30 +16,54 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+        public int PrestigeCof = 1;
+        public Random rnd = new Random();
         public int clicks = 0;
+        public int shopPoint = 0;
         public int timer = 10;
         public int upg = 1;
         public int price2X = 30;
         public int price1CLk = 10;
+        public int priceGame = 250;
+        public int exp = 0;
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            clicks+=upg;
+            clicks += upg*PrestigeCof;
+            exp+= 1*PrestigeCof;
+            label14.Text = "ваш опыт:"+exp;
             label1.Text = "клики:" + clicks;
+            label12.Text = "ваши клики за 1 нажатие:" + upg;
             if (timer1.Enabled)
             {
-                clicks += upg;
+                clicks += upg*PrestigeCof;
             }
-            if (clicks >= 1000)
+            if (shopPoint >= 10)
             {
-                label6.Text = "первые 1000 кликов!!!!!";
-                if (clicks>=10000)
-                {
-                    label6.Text = "первые 10,000 кликов!!!!!";
-                    if (clicks>= 1000000)
-                    {
-                        label6.Text = "первые 1,000,000 кликов!!!!!";
-                    }
-                }
+                label6.Text = "10 покупок в магазе!!!";
+                MessageBox.Show("ваша награда:+3 клика");
+                upg += 3;
+
+            }
+            if (clicks >= 10000)
+            { 
+               label6.Text = "первые 10,000 кликов!!!!!";
+               MessageBox.Show("ваша награда:+30 секунд 2х");
+                timer = 30;
+                timer1.Start();
+            }
+            if (clicks>= 1000000)
+            {
+               label6.Text = "первые 1,000,000 кликов!!!!!";
+                MessageBox.Show("ваша награда:+5 клика и +30 секунд 2х");
+                label10.Font = new Font(label10.Font, FontStyle.Strikeout);
+                upg += 5;
+                timer = 30;
+                timer1.Start();
+            }
+            if (exp >= 999)
+            {
+                button6.Visible = false;
             }
         }
 
@@ -50,6 +74,7 @@ namespace WindowsFormsApp2
             if (clicks >= price2X)
             {
                 clicks -= price2X;
+                shopPoint += 1*PrestigeCof;
                 timer1.Start();
                 price2X *= 2;
                 label4.Text = price2X + " кликов";
@@ -80,15 +105,66 @@ namespace WindowsFormsApp2
             if (clicks>=price1CLk)
             {
                 clicks -= price1CLk;
-                upg++;
+                upg += 1 * PrestigeCof;
+                shopPoint++;
                 price1CLk *= 2;
                 label3.Text = price1CLk + " кликов";
                 label1.Text = "клики:" + clicks;
+                if (shopPoint>=15)
+                {
+                    label11.Font = new Font(label11.Font, FontStyle.Strikeout);
+                    MessageBox.Show("ваша награда:+20 секунд 2х");
+                    timer = 20;
+                    timer1.Start();
+                }
             }
             else
             {
                 MessageBox.Show("не хватает кликов");
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (exp>=5000 && clicks >= 100000 && shopPoint >= 20)
+            {
+                clicks = 0;
+                upg = 1;
+                shopPoint = 0;
+                price2X = 30;
+                price1CLk = 10;
+                exp = 0;
+                PrestigeCof++;
+                button4.Visible = false;
+                label13.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("вам не хватает");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (clicks >= priceGame)
+            {
+                clicks -= priceGame;
+                int num = rnd.Next(1, 5);
+                int btnNum = int.Parse(textBox1.Text);
+                if (btnNum==num)
+                {
+                    MessageBox.Show("поздравляю,вы выйграли!!!");
+                    MessageBox.Show("ваша награда:+10 кликов");
+                    upg += 10;
+                }
+                priceGame += 100;
+            }
+            else
+            {
+                MessageBox.Show("вам не хватает");
+            }
+        }
+
+       
     }
 }
